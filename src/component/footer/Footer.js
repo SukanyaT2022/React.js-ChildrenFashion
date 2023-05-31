77265689cfdb634b063046ee95dda5ae62b6498d
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-05-31 07:27:19 -0400

    add

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,94 +1,126 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
 
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-
 
 import { FaRainbow } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <div>
-<div className='mainBox-footer'>
+      <div className="mainBox-footer">
 
-{/* box1 */}
-  <div className='samllBox-footer'>
-    <h2>Customer Support</h2>
-<form>
- <input type="email" placeholder='Enter your email address'/>
- <input type="submit" placeholder='Join'/>
- </form>
+        {/* box1 */}
+        <div className="samllBox-footer controlBox1">
+          <h2>Customer Support</h2>
+          <form className="controlInputBox">
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              className="controlInputEmail"
+            />
+            <input type="submit" value="Join" className="controlSubmitBox" />
+          </form>
 
- <p className='controlTextJoin'>
- *Valid for first-time registrants only & applies to reg. priced items only.
- </p>
-  
-  </div>
+          <p className="controlTextJoin">
+            *Valid for first-time registrants only & applies to reg. priced
+            items only.
+          </p>
+        </div>
 
-  {/* box2 */}
+        {/* box2 */}
 
-  <div className='samllBox-footer'>
-    <h2>Customer Support</h2>
+        <div className="samllBox-footer moveSmallBoxLeft">
+          <h2>Customer Support</h2>
 
- <ul className='controlUL'>
-<li><FaRainbow className='controlRaibowFooter'/>Refund Policy</li>
-<li><FaRainbow className='controlRaibowFooter'/>Privacy Policy</li>
-<li><FaRainbow className='controlRaibowFooter'/>FAQ</li>
-<li><FaRainbow className='controlRaibowFooter'/>Shipping</li>
+          <ul className="controlUL">
+            <li>
+              <FaRainbow className="controlRaibowFooter" />
+              Refund Policy
+            </li>
+            <li>
+              <FaRainbow className="controlRaibowFooter" />
+              Privacy Policy
+            </li>
+            <li>
+              <FaRainbow className="controlRaibowFooter" />
+              FAQ
+            </li>
+            <li>
+              <FaRainbow className="controlRaibowFooter" />
+              Shipping
+            </li>
+          </ul>
+        </div>
 
- </ul>
-  </div>
+        {/* box3 */}
 
+        <div className="samllBox-footer moveSmallBoxLeft">
+          <h2>Customer Support</h2>
 
-  {/* box3 */}
+          <ul className="controlUL">
+            <li>
+              <FaRainbow className="controlRaibowFooter" />
+              Refund Policy
+            </li>
+            <li>
+              <FaRainbow className="controlRaibowFooter" />
+              Privacy Policy
+            </li>
+            <li>
+              <FaRainbow className="controlRaibowFooter" />
+              FAQ
+            </li>
+            <li>
+              <FaRainbow className="controlRaibowFooter" />
+              Shipping
+            </li>
+          </ul>
+        </div>
 
-  <div className='samllBox-footer'>
-    <h2>Customer Support</h2>
+        {/* box4 */}
 
- <ul className='controlUL'>
-<li><FaRainbow className='controlRaibowFooter'/>Refund Policy</li>
-<li><FaRainbow className='controlRaibowFooter'/>Privacy Policy</li>
-<li><FaRainbow className='controlRaibowFooter'/>FAQ</li>
-<li><FaRainbow className='controlRaibowFooter'/>Shipping</li>
+        <div className="samllBox-footer moveSmallBoxLeft">
+          <h2>Customer Support</h2>
 
- </ul>
-  </div>
-
-
-  {/* box4 */}
-
-  <div className='samllBox-footer'>
-    <h2>Customer Support</h2>
-
- <ul className='controlUL'>
-<li><FaRainbow className='controlRaibowFooter'/>Refund Policy</li>
-<li><FaRainbow className='controlRaibowFooter'/>Privacy Policy</li>
-<li><FaRainbow className='controlRaibowFooter'/>FAQ</li>
-<li><FaRainbow className='controlRaibowFooter'/>Shipping</li>
-
- </ul>
-  </div>
-
-
-
-  
-
-
-
-
-
-
-
+          <ul className="controlUL">
+            <li>
+              <FaRainbow className="controlRaibowFooter" />
+              Refund Policy
+            </li>
+            <li>
+              <FaRainbow className="controlRaibowFooter" />
+              Privacy Policy
+            </li>
+            <li>
+              <FaRainbow className="controlRaibowFooter" />
+              FAQ
+            </li>
+            <li>
+              <FaRainbow className="controlRaibowFooter" />
+              Shipping
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* copy right section */}
+   
+<div className='copyRight'>
+<hr/>
+  <small>&copy;copyRight 2023 by Tiparpron Sukanya</small>
 
 </div>
 
 
 
 
-  
-    {/* <Row xs={1} sm={2} md={2} lg={4} className="g-4">
+
+
+
+
+      {/* <Row xs={1} sm={2} md={2} lg={4} className="g-4">
       {Array.from({ length: 4 }).map((_, idx) => (
         <Col>
           <Card>
@@ -105,10 +137,8 @@ const Footer = () => {
         </Col>
       ))}
     </Row> */}
-
     </div>
-  )
-}
+  );
+};
 
-export default Footer
-
+export default Footer;
