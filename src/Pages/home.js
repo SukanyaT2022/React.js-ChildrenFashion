c9ce9303d5815e6010402df9c6a4701af0fe1237
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-06-27 07:37:55 -0400

    add

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -3,16 +3,47 @@ import './home.css'
 import Navbar from './../component/header/Navbar'
 import Layout from '../component/Layout/layout'
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 const Home = () => {
   return (
     <>
 <Layout>
+<div className='mainBox-sectionOne'>
+  <div className='control-redBox'>
+    <h3>Up to 50% off Summer Sale</h3>
+    <h3>Discount taken from $5</h3>
 
-<p>Home</p>
+  </div>
+  <div className='control2Box-sectionOne'>
+<h1>SALE</h1>
+<h3>ONLINE AND IN STORES</h3>
+  </div>
+</div>
+{/* END SECTION 1 */}
+
+{/* start section 2 */}
+
+<div className='main-section2Home'>
+<Container>
+  
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
+
+
+</div>
+
+
+
+
 {/* line below we call component card to use on this page */}
-
-
  </Layout>
      
      </>
