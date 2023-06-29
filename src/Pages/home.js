179492de739126a25064e9a3e6f5ba2dc91b0ac4
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-06-29 07:07:45 -0400

    add

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -31,15 +31,71 @@ const Home = () => {
 <Container>
   
       <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
       </Row>
     </Container>
-
-
+</div>
+{/* text in between */}
+<div>
+  <h2 className='text-center controlh2backtSchool'>Made a back-to-school list.</h2>
 </div>
 
+{/* section 2 */}
+<div className='main-section2Home section2'>
+<Container>
+  
+      <Row>
+        <Col>
+        <img src='https://images.pexels.com/photos/4982530/pexels-photo-4982530.jpeg?auto=compress&cs=tinysrgb&w=800' />
+        <p>+ NEW ARRIVAL</p>
+        </Col>
+
+        <Col>
+        <img src = 'https://images.pexels.com/photos/5088188/pexels-photo-5088188.jpeg?auto=compress&cs=tinysrgb&w=800'/>
+        <p>PROJECT GAP &gt;</p>
+        </Col>
+      
+      </Row>
+    </Container>
+</div>
+
+{/* section 3 */}
+
+{/* text in between */}
+<div>
+  <h2 className='text-center py-4 controlh2backtSchool'>Sunrise to Sunset</h2>
+</div>
+
+{/* section 3 */}
+<div className='main-section2Home section2 mb-3'>
+<Container>
+  
+      <Row>
+        <Col>
+        <img src='https://images.pexels.com/photos/5325700/pexels-photo-5325700.jpeg?auto=compress&cs=tinysrgb&w=800' />
+        <p>+ NEW ARRIVAL</p>
+        </Col>
+
+        <Col>
+        <img src = 'https://images.pexels.com/photos/5905497/pexels-photo-5905497.jpeg?auto=compress&cs=tinysrgb&w=800'/>
+        <p>VACATION SHOP &gt;</p>
+        </Col>
+
+        <Col>
+        <img src = 'https://images.pexels.com/photos/9035942/pexels-photo-9035942.jpeg?auto=compress&cs=tinysrgb&w=800'/>
+        <p>BOY &gt;</p>
+        </Col>
+
+        <Col>
+        <img src = 'https://images.pexels.com/photos/189857/pexels-photo-189857.jpeg?auto=compress&cs=tinysrgb&w=800'/>
+        <p>GIRL &gt;</p>
+        </Col>
+      
+      </Row>
+    </Container>
+</div>
 
 
 
