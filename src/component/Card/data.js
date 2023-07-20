8ebf72d7751217cabc770e85d69836f126e469db
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-07-19 22:08:46 -0400

    add

--- src/component/Card/data.js
+++ src/component/Card/data.js
@@ -1,56 +1,56 @@
 export const shirt = [
     {
         id:1,
-        name:"shirt1",
+        name:"T Shirt Multi",
         reviews: "",
         price: 30,
         discountPrice: 20,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2hpbGRyZW4lMjBmYXNoaW9ufGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=700&q=60"
+        url: "https://images.unsplash.com/photo-1662142768575-7076c5bd2c3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGtpZCUyMHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
     {
         id:2,
-        name:"shirt2",
+        name:"Graphic T-Shirt",
         reviews: "",
         price: 30,
         discountPrice: 15,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1631006456205-ae8bd66c3b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGdpcmwlMjB0JTIwc2hpcnQlMjBraWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2019/04/18/00/16/boy-4135807_1280.jpg"
 
 
     },
     {
         id:3,
-        name:"shirt3",
+        name:"Simple T-Shirt",
         reviews: "",
         price: 30,
         discountPrice: 25,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1603200927495-b773742e6630?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGdpcmwlMjB0JTIwc2hpcnQlMjBraWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2021/09/13/05/34/kid-6620283_1280.jpg"
 
 
     },
     {
         id:4,
-        name:"shirt4",
+        name:"Happy T-Shirt",
         reviews: "",
         price: 20,
         discountPrice: 10,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1503342484812-ee33283508a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdpcmwlMjB0JTIwc2hpcnQlMjBraWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+        url: "https://images.unsplash.com/photo-1463674349210-38e4fa154dda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHQlMjBzaGlydCUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
     {
         id:5,
-        name:"shirt4",
+        name:"Recycled Tee",
         reviews: "",
         price: 25,
         discountPrice: 15,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1503342484812-ee33283508a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdpcmwlMjB0JTIwc2hpcnQlMjBraWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2020/05/17/22/26/girl-5183752_1280.jpg"
     },
     {
         id:6,
-        name:"shirt4",
+        name:"Band Graphic Tee",
         reviews: "",
         price: 25,
         discountPrice: 10,
@@ -59,21 +59,21 @@ export const shirt = [
     },
     {
         id:7,
-        name:"shirt4",
+        name:"Red T-Shirt",
         reviews: "",
         price: 10,
         discountPrice: 8,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1503342484812-ee33283508a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdpcmwlMjB0JTIwc2hpcnQlMjBraWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+        url: "https://images.unsplash.com/photo-1659475820941-d93c4c2d43d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHQlMjBzaGlydCUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
     {
         id:8,
-        name:"shirt4",
+        name:"Organic Cotton T-Shirt",
         reviews: "",
         price: 25,
         discountPrice: 15,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1503342484812-ee33283508a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdpcmwlMjB0JTIwc2hpcnQlMjBraWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2016/04/23/10/45/child-1347388_1280.jpg"
     }
 
 
@@ -83,61 +83,61 @@ export const dress = [
 
     {
         id:22,
-        name:"dress1",
+        name:"Barbie Dress",
         reviews: "",
         price: 30,
         discountPrice: 15,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1498945247205-66f185f73830?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Z2lybCUyMGRyZXNzJTIwa2lkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2014/10/31/07/05/girl-510451_1280.jpg"
 
 
     },
     {
         id:23,
-        name:"dress2",
+        name:"Happy Dress",
         reviews: "",
         price: 30,
         discountPrice: 15,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1518155481150-22002b67ac88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGdpcmwlMjBkcmVzcyUyMGtpZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2021/04/12/21/46/girl-6174060_1280.jpg"
 
 
     },
     {
         id:24,
-        name:"dress3",
+        name:"Summer Dress",
         reviews: "",
         price: 35,
         discountPrice: 20,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1508083460982-28b3207400d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybCUyMGRyZXNzJTIwa2lkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2016/11/29/04/23/camera-1867296_1280.jpg"
 
 
     },
     {
         id:25,
-        name:"dress4",
+        name:"Sun Dress",
         reviews: "",
         price: 35,
         discountPrice: 25,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1474325585148-23f982efe44c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z2lybCUyMGRyZXNzJTIwa2lkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2020/08/03/14/05/girl-5460168_1280.jpg"
 
 
     },
     {
         id:26,
-        name:"dress4",
+        name:"White Dress",
         reviews: "",
         price: 30,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1474325585148-23f982efe44c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z2lybCUyMGRyZXNzJTIwa2lkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2020/12/20/09/08/girl-5846483_1280.jpg"
 
 
     },
     {
         id:27,
-        name:"dress4",
+        name:"Linen Dress",
         reviews: "",
         price: 40,
         discountPrice: 25,
@@ -148,23 +148,23 @@ export const dress = [
     },
     {
         id:28,
-        name:"dress4",
+        name:"Floral Dress",
         reviews: "",
         price: 20,
         discountPrice: 10,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1474325585148-23f982efe44c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z2lybCUyMGRyZXNzJTIwa2lkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2017/06/22/23/22/dance-2432910_1280.jpg"
 
 
     },
     {
         id:29,
-        name:"dress4",
+        name:"Pink Dress",
         reviews: "",
         price: 40,
         discountPrice: 20,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1474325585148-23f982efe44c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z2lybCUyMGRyZXNzJTIwa2lkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2019/07/05/15/07/summer-4318826_1280.jpg"
 
 
     }
@@ -176,84 +176,84 @@ export const dress = [
 export const accesories = [
     {
         id:33,
-        name:"accesories1",
+        name:"Bunny Headband",
         reviews: "",
         price: 30,
         discountPrice: 15,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1484820540004-14229fe36ca4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dG95JTIwa2lkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2021/03/25/21/16/girl-6124237_1280.jpg"
 
 
     },
     {
         id:34,
-        name:"accesories2",
+        name:"Sun Hat",
         reviews: "",
         price: 30,
         discountPrice: 25,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1485460535564-844461f37f77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHRveSUyMGtpZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2021/05/14/01/56/child-6252212_1280.jpg"
 
 
     },
     {
         id:35,
-        name:"accesories3",
+        name:"Fun Bracelets",
         reviews: "",
         price: 20,
         discountPrice: 15,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2hpbGRyZW4lMjBmYXNoaW9ufGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=700&q=60"
+        url: "https://cdn.pixabay.com/photo/2015/11/11/02/36/hands-1038103_1280.jpg"
     },
     {
         id:36,
-        name:"accesories4",
+        name:"Watch",
         reviews: "",
         price: 20,
         discountPrice: 15,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2hpbGRyZW4lMjBmYXNoaW9ufGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=700&q=60"
+        url: "https://cdn.pixabay.com/photo/2017/08/25/16/14/accessories-2680585_1280.jpg"
 
     },
     {
         id:37,
-        name:"accesories1",
+        name:"Sunglasses",
         reviews: "",
         price: 30,
         discountPrice: 15,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1484820540004-14229fe36ca4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dG95JTIwa2lkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        url: "https://cdn.pixabay.com/photo/2016/10/24/04/19/summer-1765351_1280.jpg"
 
 
     },
     {
         id:38,
-        name:"accesories2",
+        name:"Necklaces",
         reviews: "",
         price: 30,
         discountPrice: 20,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1485460535564-844461f37f77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHRveSUyMGtpZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+        url: "https://images.unsplash.com/photo-1506630448388-4e683c67ddb0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGpld2Vscnl8ZW58MHwxfDB8fHww&auto=format&fit=crop&w=800&q=60"
 
 
     },
     {
         id:39,
-        name:"accesories3",
+        name:"Cool Rings",
         reviews: "",
         price: 35,
         discountPrice: 20,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2hpbGRyZW4lMjBmYXNoaW9ufGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=700&q=60"
+        url: "https://images.unsplash.com/photo-1631982690223-8aa4be0a2497?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGpld2Vscnl8ZW58MHwxfDB8fHww&auto=format&fit=crop&w=800&q=60"
     },
     {
         id:40,
-        name:"accesories4",
+        name:"Gold Bracelet",
         reviews: "",
         price: 30,
         discountPrice: 25,
         season: "Sales",
-        url: "https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2hpbGRyZW4lMjBmYXNoaW9ufGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=700&q=60"
+        url: "https://images.unsplash.com/photo-1602173574767-37ac01994b2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8amV3ZWxyeXxlbnwwfDF8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
 
     }
 ]
